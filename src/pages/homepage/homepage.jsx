388546912ea64b1a3dffc04118e37cc4f9313cd8
Keren Villatoro <kerenv@outlook.com>
2024-11-26 15:28:05 -0800

    homepage

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import ProductPage from "../../components/product/product"
+
+function HomePage() {
+  return (
+    <div>HomePage
+      <ProductPage />
+    </div>
+  )
+}
+
+export default HomePage
